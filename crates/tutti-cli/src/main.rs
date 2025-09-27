ddaf093a44bccbf620d1ce9c34e78a0fc2c75bf7
Suprun <hello@syoma.website>
2025-09-27 20:35:52 +0400

    feat(cli): add logs printing

--- crates/tutti-cli/src/main.rs
+++ crates/tutti-cli/src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use clap::Parser;
+use futures::StreamExt;
 use tutti_config::load_from_path;
 use tutti_core::{CommandSpec, ProcessManager, UnixProcessManager};
 
@@ -10,6 +11,7 @@ async fn main() -> Result<()> {
     let cli = config::Cli::parse();
 
     let mut process_manager = UnixProcessManager::new();
+    let mut running = Vec::new();
 
     match cli.command {
         config::Commands::Run { file } => {
@@ -19,14 +21,25 @@ async fn main() -> Result<()> {
             for (key, service) in project.services {
                 println!("Running service {key}");
 
-                process_manager
+                let mut spawned = process_manager
                     .spawn(CommandSpec {
-                        name: key,
+                        name: key.clone(),
                         cmd: service.cmd,
                         cwd: None,   // TODO
                         env: vec![], // TODO
                     })
                     .await?;
+                let task = tokio::spawn(async move {
+                    while let Some(line) = spawned.stdout.next().await {
+                        let s = String::from_utf8(line).unwrap_or_default();
+                        print!("[{}] {}", key, s);
+                    }
+                });
+                running.push(task);
+            }
+
+            for task in running {
+                task.await.unwrap();
             }
         }
     }
