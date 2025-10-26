7210809176fc22b42d6e271e806d99708c15e6fc
Suprun <hello@syoma.website>
2025-10-26 21:37:32 +0400

    chore: add more tests (#18)

--- crates/tutti-transport/src/client/ipc_client.rs
+++ crates/tutti-transport/src/client/ipc_client.rs
@@ -156,3 +156,101 @@ impl IpcClient {
         Ok(stream)
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use tokio::task;
+
+    fn new_client() -> (
+        IpcClient,
+        Receiver<(TuttiMessage, mpsc::Sender<TuttiMessage>)>,
+    ) {
+        let (tx, rx) = mpsc::channel::<(TuttiMessage, mpsc::Sender<TuttiMessage>)>(BUFFER_SIZE);
+        (
+            IpcClient {
+                _task: tokio::spawn(async move {}),
+                in_socket: tx,
+                message_counter: 0,
+            },
+            rx,
+        )
+    }
+
+    #[tokio::test]
+    async fn test_send() {
+        let (mut client, mut rx) = new_client();
+
+        let server = task::spawn(async move {
+            let (req, resp_tx) = rx.recv().await.expect("request");
+            assert_eq!(req.id, 1);
+            assert!(matches!(req.req_type, MessageType::Request));
+            assert!(matches!(req.body, TuttiApi::Ping));
+
+            let response = TuttiMessage {
+                id: req.id,
+                req_type: MessageType::Response,
+                body: TuttiApi::Ping,
+            };
+            resp_tx.send(response).await.unwrap();
+        });
+
+        let res = client.send(TuttiApi::Ping).await;
+        server.await.unwrap();
+
+        assert!(matches!(res, Ok(TuttiApi::Ping)));
+    }
+
+    #[tokio::test]
+    async fn test_send_ignore_mismatched_ids() {
+        let (mut client, mut rx) = new_client();
+
+        let server = task::spawn(async move {
+            let (req, resp_tx) = rx.recv().await.expect("request");
+
+            let wrong = TuttiMessage {
+                id: req.id + 42,
+                req_type: MessageType::Response,
+                body: TuttiApi::Ping,
+            };
+            resp_tx.send(wrong).await.unwrap();
+
+            let ok = TuttiMessage {
+                id: req.id,
+                req_type: MessageType::Response,
+                body: TuttiApi::Ping,
+            };
+            resp_tx.send(ok).await.unwrap();
+        });
+
+        let res = client.send(TuttiApi::Ping).await;
+        server.await.unwrap();
+
+        assert!(matches!(res, Ok(TuttiApi::Ping)));
+    }
+
+    #[tokio::test]
+    async fn test_ping() {
+        let (mut client, mut rx) = new_client();
+
+        let server = task::spawn(async move {
+            let (req, resp_tx) = rx.recv().await.expect("request");
+            assert_eq!(req.id, 1);
+            assert!(matches!(req.req_type, MessageType::Request));
+            assert!(matches!(req.body, TuttiApi::Ping));
+
+            let response = TuttiMessage {
+                id: req.id,
+                req_type: MessageType::Response,
+                body: TuttiApi::Ping,
+            };
+            resp_tx.send(response).await.unwrap();
+        });
+
+        let res = client.ping().await;
+        server.await.unwrap();
+
+        assert!(res);
+    }
+}
